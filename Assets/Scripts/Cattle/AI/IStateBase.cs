9bdee11e7f74ce30afe5adf055ab44efb92c2502
Jimmy Mottini <mottinijimmy@gmail.com>
2018-08-11 10:16:26 -0400

    Added State Machine

---
+++ Assets/Scripts/Cattle/AI/IStateBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cattle.Interfaces
+{
+    public interface IStateBase
+    {
+        void StateUpdate();
+    }
+}
---
+++ Assets/Scripts/Cattle/AI/States/BeginState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cattle.States
+{
+    public class BeginState : Interfaces.IStateBase
+    {
+        private StateManager manager;
+
+        public BeginState(StateManager manager)
+        {
+            this.manager = manager;
+        }
+
+        public void StateUpdate()
+        {
+            manager.SwitchState(new BeginState(manager));
+        }
+    }
+}

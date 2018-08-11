0b64bd809357543cb7083a08421ba7f2346d3b12
Jesús González <ito123456789@gmail.com>
2018-08-11 18:28:03 -0400

    Working on bomb physics.

---
+++ Assets/Scripts/Cattle/Physics/ProjectilePhysicsObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Cattle.Stats;
+
+namespace Cattle.Physics
+{
+    public class ProjectilePhysicsObject : BasePhysicsObject
+    {
+        [Header("Projectile")]
+        
+        public float speed = 10.0f;
+        public float angle = 27.0f;
+
+        bool fired = false;
+
+        ProjectileStats stats;
+
+        void Awake()
+        {
+            stats = GetComponent<ProjectileStats>();
+        }
+
+        protected override void ComputeVelocity()
+        {
+            Vector2 move = Vector2.zero;
+
+            move.x = speed * stats.initialSpeedMultiplier * Mathf.Cos(angle * Mathf.Deg2Rad);
+
+            if (!fired)
+            {
+                velocity.y = speed * stats.initialSpeedMultiplier * Mathf.Sin(angle * Mathf.Deg2Rad);
+                fired = true;
+            }
+
+            TargetVelocity = move;
+        }
+    }
+}
---
+++ Assets/Scripts/Cattle/Stats/ProjectileStats.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Cattle.Stats
+{
+    public class ProjectileStats : BaseStats
+    {
+        [Header("Projectile")]
+        
+        public float range;
+        
+        [Range(0.0f, 1.0f)]
+        public float initialSpeedMultiplier; // Set this when instantiating the projectile
+    }
+}

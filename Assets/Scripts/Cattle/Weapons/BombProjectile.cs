6399948f56f85090ab0e226ae874db43671042f2
Jesús González <ito123456789@gmail.com>
2018-08-12 14:06:18 -0400

    Added explosion to bomb.

--- Assets/Scripts/Cattle/Weapons/BombProjectile.cs
+++ Assets/Scripts/Cattle/Weapons/BombProjectile.cs
@@ -7,36 +7,24 @@ namespace Cattle.Weapons
     public class BombProjectile : BaseProjectile
     {
         [Header("Bomb Projectile")]
-        public float splashRange;
+        public GameObject explosionPrefab;
+
+        public float lifeTime = 5.0f;
+
+        void Start()
+        {
+            Invoke(nameof(Explode), lifeTime);
+        }
 
         protected override void OnCollisionEnter2D(Collision2D other)
         {
-            Tilemap tilemap = other.gameObject.GetComponent<Tilemap>();
-            
-            Vector3 hitPosition = Vector3.zero;
-            if (tilemap != null)
-            {
-                foreach (ContactPoint2D hit in other.contacts)
-                {
-                    hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
-                    hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
-                    StartCoroutine(DestroyTile(tilemap, hitPosition));
-                }
-            }
-            
             Explode();
-        }
-        
-        IEnumerator DestroyTile(Tilemap tilemap, Vector3 hitPosition)
-        {
-            yield return new WaitForFixedUpdate();
-            tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
-            yield return null;
         }
 
         void Explode()
         {
-            Debug.Log("EXPLOSION!");
+            GameObject explosion = Instantiate(explosionPrefab.gameObject, transform.position, transform.rotation);
+            explosion.transform.parent = null;
             
             Destroy(gameObject);
         }
